4ad06c3c97bd0896623d578088811be9278bf0ff
Scott Carr <s.carr1024@gmail.com>
2013-09-23 17:08:00 -0400

    more plots

--- python/check.py
+++ python/check.py
@@ -6,10 +6,11 @@ data = np.loadtxt("../data/B1_4000_1_rec.dat", skiprows=2)
 # print data
 # figure(1); clf()
 # plot(data[:1000,0], data[:1000,1])
-fs = 44100.0
+#fs = 44100.0
+fs = 48000
 N = float(data.shape[0])
 
-X = np.fft.fft(data[:,1])
+X = np.fft.fft(data)
 freq = np.r_[0.0:fs/2.0:fs/N]
 
 figure(1); clf()
