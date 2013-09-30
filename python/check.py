b08436322b845efe43007aaa7dbbf8e3e8bd4c94
Scott Carr <s.carr1024@gmail.com>
2013-09-30 19:37:51 -0400

    make py script friendlier, more data

--- python/check.py
+++ python/check.py
@@ -1,23 +1,69 @@
 import numpy as np
 from pylab import *
 
+def doAnalysis():
+    i = 0
+    data = np.loadtxt(datasets[i]["filename"], skiprows=2)
+    X, freq = getFFT(data, datasets[i]["fs"])
+    makePlots(X, freq, data.shape[0])
+
+
+def getFFT(sig, fs):
+    N = float(sig.shape[0])
+    X = np.fft.fft(sig[:,1])
+    freq = np.r_[0.0:fs/2.0:fs/N]
+    return X, freq
+    
+def makePlots(sig, freq, N):
+    figure(1); clf()
+    subplot("211")
+    semilogy(freq[0:N/2], np.abs(sig[0:N/2]))
+    xlim([0, 20])
+    subplot("212")
+    semilogy(freq[0:N/2], np.abs(sig[0:N/2]))
+    xlim([3950, 4050])
+    # figure(2); clf()
+    # semilogy(freq[0:N/2], np.abs(sig[0:N/2]))
+
+def compareSideBands():
+    figure(1); clf()
+    for d in datasets:
+        data = np.loadtxt(d["filename"], skiprows=2)
+        N = float(data.shape[0])
+        fs = d["fs"]
+        X, freq = getFFT(data, fs)
+        lower = 0
+        upper = np.nonzero(freq > (20 - fs/N))[0][0]
+        peak = np.abs(np.max(X[lower:upper]))
+        print peak
+        coeff = 1./peak
+        # coeff = 1.
+        subplot("212")
+        semilogy(freq[0:N/2], np.abs(coeff)*np.abs(X[0:N/2]))
+        xlim([3950, 4050])
+        subplot("211")
+        semilogy(freq[0:N/2], np.abs(coeff)*np.abs(X[0:N/2]))
+        xlim([0, 20])
+    legend(["healthy", "healthy", "cracked"])
+
+def compare10Hz():
+    figure(2); clf()
+    for d in datasets:
+        data = np.loadtxt(d["filename"], skiprows=2)
+        N = data.shape[0]
+        fs = d["fs"]
+        X, freq = getFFT(data, fs)
+        semilogy(freq[0:N/2], np.abs(X[0:N/2]))
+        xlim([0, 20])
+
+        
+                    
+    
 ion()
-data = np.loadtxt("../data/B1_4000_1_rec.dat", skiprows=2)
-# print data
-# figure(1); clf()
-# plot(data[:1000,0], data[:1000,1])
-#fs = 44100.0
-fs = 48000
-N = float(data.shape[0])
+datasets = [{"filename": "../data/B1_4000_1.dat", "fs" : 44100.0},\
+            {"filename": "../data/B2_4000_1.dat", "fs" : 44100.0},\
+            {"filename": "../data/B3_4000_1.dat", "fs" : 44100.0}]
+#doAnalysis()
+compareSideBands()
+#compare10Hz()
 
-X = np.fft.fft(data)
-freq = np.r_[0.0:fs/2.0:fs/N]
-
-figure(1); clf()
-subplot("211")
-semilogy(freq[0:N/2], np.abs(X[0:N/2]))
-xlim([0, 20])
-
-subplot("212")
-semilogy(freq[0:N/2], np.abs(X[0:N/2]))
-xlim([3950, 4050])
